5b5de4791f0e32726e85ea4351d9cab5aecc7925
Rik <1439713475@qq.com>
2025-12-07 23:27:39 +0800

    chore: upgrade

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 // import { Provider } from "react-redux";
 // import { store } from "@/store";
 // import { MessageApiProvider } from "@/hooks";
-import "@ant-design/v5-patch-for-react-19";
 
 createRoot(document.getElementById("root")!).render(
 	// <Provider store={store}>
